5a249be8bc36a0406e09e36af73d6910a5ea790c
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-27 13:37:57 +0100

    Fonctionnalité : ajout de nouvelle tentative OK

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Game.css'
 import RowToFind from './RowToFind';
 import Combinaison from './Combinaison';
@@ -7,62 +7,62 @@ import Tentative from './Tentative';
 
 
 
-class Game extends Component{
-    constructor(props){
+class Game extends Component {
+    constructor(props) {
         super(props)
         this.case = 0
-        this.state = {  
-            toFind:this.GenerateRowToFind(),
-            combinaison:this.GenerateRowCombinaison(),
-            tentatives: [],         
+        this.state = {
+            toFind: this.GenerateRowToFind(),
+            combinaison: this.GenerateRowCombinaison(),
+            tentatives: [],
         }
     }
 
-    colors = ["OrangeRed", "RoyalBlue", "SeaGreen","gold"]
+    colors = ["OrangeRed", "RoyalBlue", "SeaGreen", "gold"]
 
     GenerateRowToFind = () => {
-        const row =[]
-        for (let i=0 ; i<4  ; i++){
-            row.push({color:this.colors[Math.floor(4*Math.random())], border:'1px black solid'})
+        const row = []
+        for (let i = 0; i < 4; i++) {
+            row.push({ color: this.colors[Math.floor(4 * Math.random())], border: '1px black solid' })
         }
         return row
     }
 
     GenerateRowCombinaison = () => {
-        const row =[]
-        for (let i=0 ; i<4  ; i++){
-            row.push({color:'SeaShell ', border:'1px black solid'})
+        const row = []
+        for (let i = 0; i < 4; i++) {
+            row.push({ color: 'SeaShell ', border: '1px black solid' })
         }
         return row
     }
 
-    changeColor =(newColor)=>{
+    changeColor = (newColor) => {
         let combClone = [...this.state.combinaison];
-        let caseValue = {...combClone[this.case], color: newColor};
+        let caseValue = { ...combClone[this.case], color: newColor };
         combClone[this.case] = caseValue;
-        this.setState({combinaison: combClone});
-        if(this.case===3) {
+        if (this.case === 3) {
             this.case = 0
             this.setState({
-                tentatives: [...this.state.tentatives, this.state.combinaison],
-                combinaison:this.GenerateRowCombinaison()
+                tentatives: [...this.state.tentatives, combClone],
+                combinaison: this.GenerateRowCombinaison()
             })
         } else {
-            this.case +=1;
-        } 
+            this.setState({ combinaison: combClone });
+            this.case += 1;
+        }
     }
 
 
-    render(){
-        const {tentatives} = this.state;
+    render() {
+        const { tentatives } = this.state;
         return (
             <div>
-                <RowToFind toFind={this.state.toFind}/>
-                {tentatives.map((item,i) => {
-                    return <Tentative combinaison={item} key={i}/>
+                <RowToFind toFind={this.state.toFind} />
+                {tentatives.map((item, i) => {
+                    return <Tentative combinaison={item} key={i} />
                 })}
-                <Combinaison combinaison={this.state.combinaison}/>
-                <Clavier colors={this.colors} changeColor={this.changeColor}/>
+                <Combinaison combinaison={this.state.combinaison} />
+                <Clavier colors={this.colors} changeColor={this.changeColor} />
             </div>
         )
     }
