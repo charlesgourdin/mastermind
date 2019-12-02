2889d3575d00c821f44729748690e842e736fd15
Charles Gourdin <gourdin.charles@gmail.com>
2019-12-02 17:59:02 +0100

    Mise en place de router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Game from './components/Game';
+import { BrowserRouter, Route } from 'react-router-dom'
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      <BrowserRouter>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/game" component={Game} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './Game.css'
+
+class HomePage extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <p style={{color:'white'}}>HomePage</p>
+            </div>
+        )
+    }
+}
+
+export default HomePage
